1419d1ef2b45348afda4412b3853aab6287ff982
iamgroot03 <deepp5113@gmail.com>
2023-11-22 18:46:19 +0530

    Image Load from api link

--- app/src/main/java/com/example/apiscalling/userAdapter.java
+++ app/src/main/java/com/example/apiscalling/userAdapter.java
@@ -3,10 +3,13 @@ package com.example.apiscalling;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -27,6 +30,9 @@ public class userAdapter extends RecyclerView.Adapter<userAdapter.userHolder> {
     @Override
     public void onBindViewHolder(@NonNull userHolder holder, int position) {
         holder.itemText.setText(userList.get(position).getTitle());
+        Picasso.get().load(userList.get(position).getThumbnailUrl()).into(holder.imageView);
+
+
     }
 
     @Override
@@ -36,10 +42,12 @@ public class userAdapter extends RecyclerView.Adapter<userAdapter.userHolder> {
 
     class userHolder extends RecyclerView.ViewHolder{
         TextView itemText;
+        ImageView imageView;
 
         public userHolder(@NonNull View itemView) {
             super(itemView);
-            itemText=itemView.findViewById(R.id.textView);
+            itemText=itemView.findViewById(R.id.title_txt);
+            imageView=itemView.findViewById(R.id.img);
 
         }
     }
